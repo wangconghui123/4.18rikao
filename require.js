31f4b03bd84294c26ba1c8b51244c0b7bc6b0cc1
wch <1114590177@qq.com>
2019-04-18 08:50:07 +0800

    完成

--- require.js
+++ require.js
@@ -4,6 +4,7 @@ function require() {
         code: "1",
         res: "aaa"
     }]
+    res.end({ code: "2", res: "dsdf" });
 }
 
 function require() {
@@ -11,4 +12,5 @@ function require() {
         code: "2",
         res: "bbbb"
     }]
+    res.end({ code: "3", res: "sdfdg" });
 }
