b370fcc1e3e67f191507f04732f7adbd21f2f4e7
wch <1114590177@qq.com>
2019-04-18 08:48:39 +0800

    引入模块

---
+++ require.js
@@ -0,0 +1,14 @@
+function require() {
+    //引入模块
+    data[{
+        code: "1",
+        res: "aaa"
+    }]
+}
+
+function require() {
+    data[{
+        code: "2",
+        res: "bbbb"
+    }]
+}
